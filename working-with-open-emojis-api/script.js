8495b32a8153b8c325d8835a5e6111d16dfc14e0
menegass0 <victormenegasso@unisantos.br>
2023-03-07 16:08:01 -0300

    prototipo-inicial-da-pesquisa-geral

--- working-with-open-emojis-api/script.js
+++ working-with-open-emojis-api/script.js
@@ -1,12 +1,16 @@
+let showArea = document.querySelector('.show-area');
+
 let arrEmoji = [];
 let arrEmojiTemp = [];
 
 //removerItems();
 verificarDadosExistentes();
-pesquisa();
+teste();
 
 function montarArray(item, indice) {
-    arrEmojiTemp[indice] = item;
+    if(indice < 30){
+        arrEmojiTemp[indice] = item;
+    }
 }
 
 function armazenarDados(data) {
@@ -14,7 +18,7 @@ function armazenarDados(data) {
 }
 
 function buscarEmojis() {
-    fetch('https://emoji-api.com/emojis?access_key=990812c361e8365cc4c033befd8cda999a58748d')
+    fetch('https://emoji-api.com/emojis?access_key=15c4485b1dc1618bff37567d3bf8e3175d6e4402')
     .then(res => res.json())
     .then(data => {
         data.forEach(
@@ -27,12 +31,15 @@ function buscarEmojis() {
 }
 
 function verificarDadosExistentes() {
-    arrEmoji = JSON.parse(localStorage.getItem('arrEmoji'));
-   
+    console.log(localStorage);
+    
+    if(localStorage.getItem('arrEmoji')){
+        arrEmoji = JSON.parse(localStorage.getItem('arrEmoji'));
+    }
+    
     if(arrEmoji.length == 0){
         console.log('funçao verificar dados existentes');
         buscarEmojis();
-        console.log(arrEmoji.length);
     }
 }
 
@@ -40,10 +47,65 @@ function removerItems() {
     localStorage.removeItem('arrEmoji');
 }
 
-function pesquisa(){
+
+
+
+
+function pesquisaTodos(){
+    showArea.innerHTML = '';
     arrEmoji.forEach(element => {
-        if(element.group == 'travel-places'){
-            console.log('asd');
-        }
+        let splitAr = element.codePoint.split(' ');
+        showArea.innerHTML += `<div class='small-emoji'> `
+        splitAr.forEach(code =>{
+            showArea.innerHTML += `
+                &#x${code};
+            `
+        });
+        showArea.innerHTML += `</div>`
     });
+}
+
+// function gerarViewEmoji(indice){
+//     let contador = 0;
+//     let splitAr = null;
+//     let tdTable = document.getElementById('grupo-emoji');
+//     let trTable = '';
+//     let codePoint = '';
+
+//     while(contador < 5){
+//         trTable = '<tr>';
+//         splitAr = arrEmoji[indice+contador].codePoint.split(' ');
+//         splitAr.forEach(code => {
+//             codePoint += `
+//                 &#x${code}
+//             `
+//         });
+        
+//         trTable += gerarTd(codePoint);
+
+//         trTable += '</tr>';
+       
+//         contador++;
+//     }
+    
+//     tdTable.innerHTML =  tdTable.innerHTML+trTable;
+
+//     setTimeout(function(){
+        
+//     },1000);
+
+// }
+
+// function gerarTd(data){
+//     return '<td>'+data+'</td>';
+// }
+
+function teste(){
+    for (let index = 0; index < 500; index++) {
+        showArea.innerHTML += `
+            <div class='emoji-test'>
+                &#x${arrEmoji[index].codePoint}
+            </div>
+        `        
+    }
 }
